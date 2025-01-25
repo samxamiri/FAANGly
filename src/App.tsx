fd04e5b41b2b030135af7ca36abb101d83ccefa5
Sam Amiri <samamiri@hotmail.com>
2025-01-25 14:34:39 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Hero from './components/hero'
+import './components/navbar'
+import Navbar from './components/navbar'
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    </>
+  )
+}
+
+export default App
